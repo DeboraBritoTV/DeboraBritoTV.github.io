851a42cad65c85e33f6687a4fc3794724aa5e88f
DeboraBritoTV <87839990+DeboraBritoTV@users.noreply.github.com>
2021-07-23 01:05:10 -0300

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,201 @@
+
+
+function abreAlbum(){
+  var abrir=document.querySelector(".capa");
+  var positionfixed=document.querySelector("body");
+  abrir.style.left='-100vw';
+  abrir.style.transition='.5s';
+  positionfixed.style.position='relative';
+}
+
+function voltarAlbum(){
+  var voltar=document.querySelector(".capa");
+  voltar.style.left='0';
+  voltar.style.transition='.5s';
+}
+
+function fechaFoto(){
+  var fechafoto=document.querySelector(".foto_zoom");
+  fechafoto.style.left='-100vw';
+}
+
+function abreFoto1(){
+  var abreFoto=document.querySelector(".foto_zoom");
+  var mudafoto=document.querySelector(".img_zoom");
+  var legend=document.querySelector(".legenda");
+  abreFoto.style.left='0';
+  abreFoto.style.transition='.5s';
+  mudafoto.src="Img/20210721_210205.jpg";
+  legend.innerHTML='Um dia feliz!';
+}
+
+function abreFoto2(){
+  var abreFoto=document.querySelector(".foto_zoom");
+  var mudafoto=document.querySelector(".img_zoom");
+  var legend=document.querySelector(".legenda");
+  abreFoto.style.left='0';
+  abreFoto.style.transition='.5s';
+  mudafoto.src='Img/20210721_210205.jpg';
+}
+
+function abreFoto3(){
+  var abreFoto=document.querySelector(".foto_zoom");
+  var mudafoto=document.querySelector(".img_zoom");
+  var legend=document.querySelector(".legenda");
+  abreFoto.style.left='0';
+  abreFoto.style.transition='.5s';
+  mudafoto.src='Img/20210721_210205.jpg';
+}
+
+function abreFoto4(){
+  var abreFoto=document.querySelector(".foto_zoom");
+  var mudafoto=document.querySelector(".img_zoom");
+  var legend=document.querySelector(".legenda");
+  abreFoto.style.left='0';
+  abreFoto.style.transition='.5s';
+  mudafoto.src = 'Img/20210721_210205.jpg';
+}
+
+function abreFoto5(){
+  var abreFoto=document.querySelector(".foto_zoom");
+  var mudafoto=document.querySelector(".img_zoom");
+  var legend=document.querySelector(".legenda");
+  abreFoto.style.left='0';
+  abreFoto.style.transition='.5s';
+  mudafoto.src = 'Img/20210721_210205.jpg';
+}
+
+function abreFoto6(){
+  var abreFoto=document.querySelector(".foto_zoom");
+  var mudafoto=document.querySelector(".img_zoom");
+  var legend=document.querySelector(".legenda");
+  abreFoto.style.left='0';
+  abreFoto.style.transition='.5s';
+  mudafoto.src = 'Img/20210721_210205.jpg';
+}
+
+function abreFoto7(){
+  var abreFoto=document.querySelector(".foto_zoom");
+  var mudafoto=document.querySelector(".img_zoom");
+  var legend=document.querySelector(".legenda");
+  abreFoto.style.left='0';
+  abreFoto.style.transition='.5s';
+  mudafoto.src = 'Img/20210721_210205.jpg';
+}
+
+function abreFoto8(){
+  var abreFoto=document.querySelector(".foto_zoom");
+  var mudafoto=document.querySelector(".img_zoom");
+  var legend=document.querySelector(".legenda");
+  abreFoto.style.left='0';
+  abreFoto.style.transition='.5s';
+  mudafoto.src = 'Img/20210721_210205.jpg';
+}
+
+function abreFoto9(){
+  var abreFoto=document.querySelector(".foto_zoom");
+  var mudafoto=document.querySelector(".img_zoom");
+  var legend=document.querySelector(".legenda");
+  abreFoto.style.left='0';
+  abreFoto.style.transition='.5s';
+  mudafoto.src = 'Img/20210721_210205.jpg';
+}
+
+function abreFoto10(){
+  var abreFoto=document.querySelector(".foto_zoom");
+  var mudafoto=document.querySelector(".img_zoom");
+  var legend=document.querySelector(".legenda");
+  abreFoto.style.left='0';
+  abreFoto.style.transition='.5s';
+  mudafoto.src = 'Img/20210721_210205.jpg';
+}
+
+function abreFoto11(){
+  var abreFoto=document.querySelector(".foto_zoom");
+  var mudafoto=document.querySelector(".img_zoom");
+  var legend=document.querySelector(".legenda");
+  abreFoto.style.left='0';
+  abreFoto.style.transition='.5s';
+  mudafoto.src = 'Img/20210721_210205.jpg';
+}
+
+function abreFoto12(){
+  var abreFoto=document.querySelector(".foto_zoom");
+  var mudafoto=document.querySelector(".img_zoom");
+  var legend=document.querySelector(".legenda");
+  abreFoto.style.left='0';
+  abreFoto.style.transition='.5s';
+  mudafoto.src = 'Img/20210721_210205.jpg';
+}
+
+function abreFoto13(){
+  var abreFoto=document.querySelector(".foto_zoom");
+  var mudafoto=document.querySelector(".img_zoom");
+  var legend=document.querySelector(".legenda");
+  abreFoto.style.left='0';
+  abreFoto.style.transition='.5s';
+  mudafoto.src = 'Img/20210721_210205.jpg';
+}
+
+function abreFoto14(){
+  var abreFoto=document.querySelector(".foto_zoom");
+  var mudafoto=document.querySelector(".img_zoom");
+  var legend=document.querySelector(".legenda");
+  abreFoto.style.left='0';
+  abreFoto.style.transition='.5s';
+  mudafoto.src = 'Img/20210721_210205.jpg';
+}
+
+function abreFoto15(){
+  var abreFoto=document.querySelector(".foto_zoom");
+  var mudafoto=document.querySelector(".img_zoom");
+  var legend=document.querySelector(".legenda");
+  abreFoto.style.left='0';
+  abreFoto.style.transition='.5s';
+  mudafoto.src = 'Img/20210721_210205.jpg';
+}
+
+function abreFoto16(){
+  var abreFoto=document.querySelector(".foto_zoom");
+  var mudafoto=document.querySelector(".img_zoom");
+  var legend=document.querySelector(".legenda");
+  abreFoto.style.left='0';
+  abreFoto.style.transition='.5s';
+  mudafoto.src = 'Img/20210721_210205.jpg';
+}
+
+function abreFoto17(){
+  var abreFoto=document.querySelector(".foto_zoom");
+  var mudafoto=document.querySelector(".img_zoom");
+  var legend=document.querySelector(".legenda");
+  abreFoto.style.left='0';
+  abreFoto.style.transition='.5s';
+  mudafoto.src = 'Img/20210721_210205.jpg';
+}
+
+function abreFoto18(){
+  var abreFoto=document.querySelector(".foto_zoom");
+  var mudafoto=document.querySelector(".img_zoom");
+  var legend=document.querySelector(".legenda");
+  abreFoto.style.left='0';
+  abreFoto.style.transition='.5s';
+  mudafoto.src = 'Img/20210721_210205.jpg';
+}
+
+function abreFoto19(){
+  var abreFoto=document.querySelector(".foto_zoom");
+  var mudafoto=document.querySelector(".img_zoom");
+  var legend=document.querySelector(".legenda");
+  abreFoto.style.left='0';
+  abreFoto.style.transition='.5s';
+  mudafoto.src = 'Img/20210721_210205.jpg';
+}
+
+function abreFoto20(){
+  var abreFoto=document.querySelector(".foto_zoom");
+  var mudafoto=document.querySelector(".img_zoom");
+  var legend=document.querySelector(".legenda");
+  abreFoto.style.left='0';
+  abreFoto.style.transition='.5s';
+  mudafoto.src = 'Img/20210721_210205.jpg';
+}
